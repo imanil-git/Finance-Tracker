633fab27a884f922d84d56210552deea28e3029e
Anil Rumdali Rai <anil.rai704757@gmail.com>
2026-05-21 16:03:32 +0545

    Custom KPI added

---
+++ FT-Frontend/src/components/CustomKPI.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+
+export const CustomKPI = ({
+  bgColor = "white",
+  iconSrc,
+  kpiType,
+  kpiValue,
+}) => {
+  return (
+    <>
+      <Row>
+        <Col md={4}>
+          <img className="img-fluid" src={iconSrc} alt={kpiType} />
+        </Col>
+        <Col md={7} className="ms-2">
+          <div className="border-bottom border-dark border-2">
+            <h6>{kpiType}</h6>
+          </div>
+
+          <div>
+            <h1>{kpiValue}</h1>
+          </div>
+        </Col>
+      </Row>
+    </>
+  );
+};
--- FT-Frontend/src/pages/Dashboard.jsx
+++ FT-Frontend/src/pages/Dashboard.jsx
@@ -1,10 +1,15 @@
-import {Container, Row, Col} from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
+import { DashboardChart } from "../components/DashboardChart";
 
 const Dashboard = () => {
   return (
     <Container className="p-5">
-      <Row className="bd-dark p-5 rounded">
-        <Col md={6}>TODO Dashboard</Col>
+      <Row className="bg-dark p-5 rounded">
+        <Col md={6}>
+          <h2>Dashboard</h2>
+        </Col>
+        <hr />
+        <DashboardChart />
       </Row>
     </Container>
   );
